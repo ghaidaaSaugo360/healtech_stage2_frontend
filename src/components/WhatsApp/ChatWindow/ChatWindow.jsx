28a7cf60482f8ec54a7ab8f9ef330242ca7930ec
ghaidaaSaugo360 <gnj03@mail.aub.edu>
2023-12-21 02:05:53 +0200

    wss

--- src/components/WhatsApp/ChatWindow/ChatWindow.jsx
+++ src/components/WhatsApp/ChatWindow/ChatWindow.jsx
@@ -165,7 +165,7 @@ function ChatWindow({ selectedContact }) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const ws = new WebSocket(`ws://${API_BASE_URL}/ws/whatsapp/`); 
+    const ws = new WebSocket(`wss://${API_BASE_URL}/ws/whatsapp/`); 
     setSocket(ws);
 
     return () => {
